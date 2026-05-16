13f43c1365bdd6aa9a28419175145628ce83b87c
Rodion Iamshanov <roiamshanov@gmail.com>
2026-05-16 19:42:29 +0200

    add: two new routers

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 from fastapi import FastAPI
-from routers.auth import router
+from routers.auth import router as auth_router
+from routers.transactions import router as transactions_router
+from routers.categories import router as categories_router
 
 app = FastAPI()
 
 
-app.include_router(router)
+app.include_router(auth_router)
+app.include_router(transactions_router)
+app.include_router(categories_router)
