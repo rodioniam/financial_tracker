de45175db2390090e883fc656867edf76de8d4a9
Rodion Iamshanov <roiamshanov@gmail.com>
2026-05-23 15:38:13 +0200

    add: rate limit for api

--- main.py
+++ main.py
@@ -2,8 +2,15 @@ from fastapi import FastAPI
 from routers.auth import router as auth_router
 from routers.transactions import router as transactions_router
 from routers.categories import router as categories_router
+from slowapi import _rate_limit_exceeded_handler
+from limiter import limiter
+
+from slowapi.errors import RateLimitExceeded
+
 
 app = FastAPI()
+app.state.limiter = limiter
+app.add_exception_handler(RateLimitExceeded, _rate_limit_exceeded_handler)
 
 
 app.include_router(auth_router)
