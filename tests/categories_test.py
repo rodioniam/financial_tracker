6adb982a4e0bc3f3a0cb986fa9280bcb0ad85142
Rodion Iamshanov <roiamshanov@gmail.com>
2026-05-23 15:50:00 +0200

    add: test for cache

--- tests/categories_test.py
+++ tests/categories_test.py
@@ -1,4 +1,10 @@
 import pytest
+# для тестирования редис импортировал данный модуль, позволяющий подменять объекты для теста
+from unittest.mock import patch
+from limiter import limiter
+
+# чтобы не было конфликта при тесте
+limiter.enabled = False
 
 
 # тест создания категорий
@@ -33,3 +39,23 @@ async def test_delete_category(auth_client):
     assert request.status_code == 201
     assert delete_request.status_code == 200
     assert check_deleteed.status_code == 404
+
+
+# тест проверки кеша
+@pytest.mark.asyncio
+async def test_get_categories_from_cache(auth_client, fake_redis_client):
+    # данная команда позволяет в services/categories заменить redis_client на фейковый клиент
+    with patch('services.categories.redis_client', fake_redis_client):
+        query = await auth_client.post(url='/categories', json={'name': 'cat_1', 'description': 'text'})
+        query_2 = await auth_client.post(url='/categories', json={'name': 'cat_2'})
+        r_user = await auth_client.get(url='/me')
+        user_id = r_user.json()['id']
+
+        r1 = await auth_client.get(url='/categories')
+        cached = await fake_redis_client.get(f'categories:{user_id}')
+        r2 = await auth_client.get(url='/categories')
+
+    assert r1.status_code == 200
+    assert cached is not None
+    assert r1.json()[0]['name'] == 'cat_1'
+    assert r2.json()[1]['name'] == 'cat_2'
