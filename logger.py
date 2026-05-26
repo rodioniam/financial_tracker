2f1d855113c75a593c5932be05af5651da9841dd
Rodion Iamshanov <roiamshanov@gmail.com>
2026-05-26 18:26:25 +0200

    add: aggregation pipeline function for getting user activity stats from logs

--- logger.py
+++ logger.py
@@ -11,3 +11,23 @@ async def log_event(event_type: str, user_id: int, details: dict):
     }
 
     await events_log.insert_one(event)
+
+
+async def get_activity(user_id):
+    pipeline = [
+        # это первый этап - фильтрация по user_id
+        {"$match": {"user_id": user_id}},
+        {
+            # это второй этап - группировка
+            '$group': {
+                '_id': '$event_type',  # по полю event_type
+                'operations_count': {  # это кастомное название для агрегации
+                    '$sum': 1  # это операция при агрегации - сумма
+                }
+            }
+        }
+    ]
+
+    query = await events_log.aggregate(pipeline)
+
+    return await query.to_list()
