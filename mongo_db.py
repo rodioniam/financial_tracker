2ca4a8b78d2e34f8267883734423aa9e72169302
Rodion Iamshanov <roiamshanov@gmail.com>
2026-05-26 18:25:10 +0200

    add: create_indexes function for mongo db

--- mongo_db.py
+++ mongo_db.py
@@ -9,3 +9,8 @@ mongo_db = AsyncMongoClient(os.environ['MONGO_URL'])  # подключение �
 db = mongo_db['fin_tracker']  # создание базы данных
 # создание коллекции, аналог таблицы. Именно это будет являться объектом для взаимодействия с базой - вставка, поиск, агрегация
 events_log = db['events_log']
+
+
+async def create_indexes():
+    await events_log.create_index('user_id')
+    await events_log.create_index('event_type')
