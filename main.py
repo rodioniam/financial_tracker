6afe657f36623788c07f14c3f64a06bd4e7d7f41
Rodion Iamshanov <roiamshanov@gmail.com>
2026-05-29 21:11:45 +0200

    add: router for analytics requests

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from routers.auth import router as auth_router
 from routers.transactions import router as transactions_router
 from routers.categories import router as categories_router
+from routers.analytics import router as analytics_router
 from slowapi import _rate_limit_exceeded_handler
 from limiter import limiter
 from mongo_db import create_indexes
@@ -23,3 +24,4 @@ app.add_exception_handler(RateLimitExceeded, _rate_limit_exceeded_handler)
 app.include_router(auth_router)
 app.include_router(transactions_router)
 app.include_router(categories_router)
+app.include_router(analytics_router)
