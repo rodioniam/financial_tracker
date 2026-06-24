2ef52273bfc369c9c4ed914cb76481a3b00e7016
Rodion Iamshanov <roiamshanov@gmail.com>
2026-06-24 16:13:33 +0200

    add: query to db with transactions now can return category name, not category_id only

--- repositories/transaction_repo.py
+++ repositories/transaction_repo.py
@@ -1,6 +1,6 @@
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy import select, delete, update
-from models import Transaction
+from models import Transaction, Category
 
 
 async def create_transaction(transaction: Transaction, session: AsyncSession):
@@ -26,18 +26,33 @@ async def update_transaction(transaction_id: int, session: AsyncSession, data: d
     await session.commit()
 
 
-async def filter_transactions(session: AsyncSession, user_id: int, date=None, category_id=None, type=None):
-    conditions = []
+# последний параметр позволяет передавать еще и название категории если нужно
+async def filter_transactions(session: AsyncSession, user_id: int, date=None, category_id=None, type=None, with_category: bool = False):
+    conditions = []  # список условий
 
     # список будет пополняться объектами SQLAlchemy
     conditions.append(Transaction.user_id == user_id)
 
+    # если условие передано в функцию, то оно добавляется в список
     if date:
         conditions.append(Transaction.date == date)
     if category_id:
         conditions.append(Transaction.category_id == category_id)
     if type:
         conditions.append(Transaction.type == type)
+
+    if with_category:
+        query = await session.execute(
+            select(
+                Transaction,
+                Category.name
+            ).join(
+                Category,
+                Transaction.category_id == Category.id
+            ).filter(*conditions)
+        )
+
+        return query.all()
 
     # метод filter() преобразует разархивированный список SQLAlchemy объектов в where условия через and
     search = await session.execute(select(Transaction).filter(*conditions))
